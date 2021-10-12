54b355abb9155eefedc13b19bcef52c8b5596c25
hwsingh27 <62118804+hwsingh27@users.noreply.github.com>
2021-10-12 08:23:37 -0600

    Add files via upload

--- source/pwm.c
+++ source/pwm.c
@@ -29,6 +29,10 @@ int r1,g1,b1;
 
 int time=0;
 
+/*
+ * Comments are added as suggested by peer Varun Mehta.
+ * Comments have been put in every configuration statements.
+ */
 void Init_Pwm(void)
 {
 	//enable clock to PORTD, PORTB, TPM0 and TPM2
@@ -56,22 +60,30 @@ void Init_Pwm(void)
 	TPM2->SC = TPM_SC_PS(1);
 	//continue operation in DEBUG mode
 	TPM2->CONF |= TPM_CONF_DBGMODE(3);
-	//set channel 1 to edge aligned low-true PWM
+	//set channel 0 to edge aligned low-true PWM
 	TPM2->CONTROLS[0].CnSC = TPM_CnSC_MSB_MASK| TPM_CnSC_ELSA_MASK;
 	//set initial duty cycle as 0
 	TPM2->CONTROLS[0].CnV = 0;
 	//start TPM
 	TPM2->SC |= TPM_SC_CMOD(1);
 
+	//set channel 1 to edge aligned low-true PWM
 	TPM2->CONTROLS[1].CnSC = TPM_CnSC_MSB_MASK| TPM_CnSC_ELSA_MASK;
+	//set initial duty cycle as 0
 	TPM2->CONTROLS[1].CnV = 0;
+	//start TPM
 	TPM2->SC |= TPM_SC_CMOD(1);
 
 	TPM0->MOD = 48000-1;
+	//set the TPM counter direction to up with the prescaler of 2
 	TPM0->SC = TPM_SC_PS(1);
+	//continue operation in DEBUG mode
 	TPM0->CONF |= TPM_CONF_DBGMODE(3);
+	//set channel 1 to edge aligned low-true PWM
 	TPM0->CONTROLS[1].CnSC = TPM_CnSC_MSB_MASK| TPM_CnSC_ELSA_MASK;
+	//set initial duty cycle as 0
 	TPM0->CONTROLS[1].CnV = 0;
+	//start TPM
 	TPM0->SC |= TPM_SC_CMOD(1);
 }
 
